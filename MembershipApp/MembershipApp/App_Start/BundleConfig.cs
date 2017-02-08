9b5cdda758af8aa5f48c8e07be1b6c2709cac318
stepforwardtech <jrstrayhorn@gmail.com>
2017-02-08 15:38:46 -0500

    Adding the admin menu

--- MembershipApp/MembershipApp/App_Start/BundleConfig.cs
+++ MembershipApp/MembershipApp/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace MembershipApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/admin").Include(
+                      "~/Scripts/AdminMenu.js"));
         }
     }
 }
