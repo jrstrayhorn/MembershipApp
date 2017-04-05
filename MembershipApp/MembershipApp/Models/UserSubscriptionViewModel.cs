14f713ac731fab327f5bb27b05e7b6b407447680
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-05 14:08:59 -0400

    Adding the Subscriptions view

---
+++ MembershipApp/MembershipApp/Models/UserSubscriptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MembershipApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MembershipApp.Models
+{
+    public class UserSubscriptionViewModel
+    {
+        public ICollection<Subscription> Subscriptions { get; set; }
+        public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
+        public bool DisableDropDown { get; set; }
+        public string UserId { get; set; }
+        public int SubscriptionId { get; set; }
+    }
+}
