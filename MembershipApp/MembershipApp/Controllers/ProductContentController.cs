fd5b78bdb0b3f5d434fd8baf1074d30727a1cecb
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-06 11:24:24 -0400

    Creating models/controller to support Product Content displays

---
+++ MembershipApp/MembershipApp/Controllers/ProductContentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace MembershipApp.Controllers
+{
+    [Authorize]
+    public class ProductContentController : Controller
+    {
+        // GET: ProductContent
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
