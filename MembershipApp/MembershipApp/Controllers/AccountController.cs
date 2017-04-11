f9cbc02c48db1a234f6a8c7362d177910c1f5a06
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-11 17:16:29 -0400

    Adding js for Login Panel

--- MembershipApp/MembershipApp/Controllers/AccountController.cs
+++ MembershipApp/MembershipApp/Controllers/AccountController.cs
@@ -773,6 +773,34 @@ namespace MembershipApp.Controllers
 
         #endregion
 
+        #region Log in
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> LoginAsync(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = UserManager.Users.FirstOrDefault(u => u.Email.Equals(model.Email));
+                if (user != null && user.UserName.Length > 0)
+                {
+                    var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
+
+                    if (result.Equals(SignInStatus.Success))
+                    {
+                        return PartialView("_LoginPanelPartial", model);
+                    }
+                }
+            }
+
+            // if we get this far something has failed and we re-display the form
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return PartialView("_LoginPanelPartial", model);
+        }
+
+        #endregion
+
         #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
--- MembershipApp/MembershipApp/Scripts/login.js
+++ MembershipApp/MembershipApp/Scripts/login.js
@@ -14,4 +14,47 @@
     function onCloseLogin() {
         $('div[data-login-user-area').removeClass('open');
     }
+
+    // wire up the login event
+    var loginButton = $("#login-button").click(onLoginClick);
+
+    function onLoginClick() {
+        var url = $('#login-button').attr('data-login-action');
+        var return_url = $('#login-button').attr('data-login-return-url');
+        var email = $('#Email').val();
+        var pwd = $('#Password').val();
+        var remember_me = $('#RememberMe').prop('checked');
+        var antiforegry = $('[name="__RequestVerificationToken"]').val();
+
+        $.post(url, { __RequestVerificationToken: antiforegry, email: email, password: pwd, RememberMe: remember_me },
+            function (data) {
+                var parsed = $.parseHTML(data);
+                var hasErrors = $(parsed).find("[data-valmsg-summary]").text().replace(/\n|\r/g, "").length > 0;
+
+                if (hasErrors == true) {
+                    $("div[data-login-panel-partial]").html(data);
+                    $('div[data-login-user-area').addClass('open');
+                    $('#Email').addClass("data-login-error");
+                    $('#Password').addClass("data-login-error");
+                }
+                else {
+                    $('div[data-login-user-area]').removeClass('open');
+                    $('#Email').removeClass("data-login-error");
+                    $('#Password').removeClass("data-login-error");
+                    location.href = return_url;
+                }
+
+                /* Wire up events */
+                loginButton = $("#login-button").click(onLoginClick);
+                loginLinkHover = $("#loginLink").hover(onLoginLinkHover);
+                loginCloseBUtton = $("#close-login").click(onCloseLogin);
+            }).fail(function (xhr, status, error) {
+                $('#Email').addClass("data-login-error");
+                $('#Password').addClass("data-login-error");
+                /* Wire up events */
+                loginButton = $("#login-button").click(onLoginClick);
+                loginLinkHover = $("#loginLink").hover(onLoginLinkHover);
+                loginCloseBUtton = $("#close-login").click(onCloseLogin);
+            });
+    }
 });
