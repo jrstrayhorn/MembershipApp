1298e0edce8a1fad026df0085181ec2fdc0bee58
stepforwardtech <jrstrayhorn@gmail.com>
2017-04-11 16:18:50 -0400

    Updating css and js for login panel

--- MembershipApp/MembershipApp/Scripts/login.js
+++ MembershipApp/MembershipApp/Scripts/login.js
@@ -6,4 +6,12 @@
     function onLoginLinkHover() {
         $('div[data-login-user-area]').addClass('open');
     };
+
+    // wire up the click event
+    var loginCloseBUtton = $("#close-login").click(onCloseLogin);
+
+    // the function called by the click event
+    function onCloseLogin() {
+        $('div[data-login-user-area').removeClass('open');
+    }
 });
